13c8fa940a6c08e140638a5691008ef035fd5fa2
fazletdinov <fvi-it@mail.ru>
2022-07-05 16:38:54 +0500

    была добавлена карта сайта

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,10 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitmap
+
+sitemaps = {'posts': PostSitmap}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ]
 
 if settings.DEBUG:
